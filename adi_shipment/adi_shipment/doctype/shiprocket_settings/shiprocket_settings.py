19b86fe3ae84234ebc67fc40b062574e6ffa1afe
Md Faiyaz Ansari <itsmdfaiyaz@gmail.com>
2025-12-08 17:17:59 +0530

    feat: Add shipment naming series setting to Shiprocket Settings and implement logic to update the Shipment DocType's autoname.

--- adi_shipment/adi_shipment/doctype/shiprocket_settings/shiprocket_settings.py
+++ adi_shipment/adi_shipment/doctype/shiprocket_settings/shiprocket_settings.py
@@ -3,7 +3,23 @@ from frappe.model.document import Document
 from adi_shipment.api.shiprocket import get_token
 
 class ShiprocketSettings(Document):
-    pass
+	def validate(self):
+		if self.shipment_naming_series:
+			self.update_shipment_autoname()
+
+	def update_shipment_autoname(self):
+		try:
+			if not frappe.db.exists("DocType", "Shipment"):
+				return
+
+			doc = frappe.get_doc("DocType", "Shipment")
+			if doc.autoname != self.shipment_naming_series:
+				doc.autoname = self.shipment_naming_series
+				doc.flags.ignore_permissions = True
+				doc.save()
+				frappe.msgprint(f"Shipment Naming Series updated to {self.shipment_naming_series}")
+		except Exception as e:
+			frappe.log_error(f"Error updating Shipment autoname: {str(e)}")
 
 @frappe.whitelist()
 def generate_token_manual():
